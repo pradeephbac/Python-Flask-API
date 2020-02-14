f0e86ec4b8539d966f8de138159a41e927b67ce8
pradeephbac <pradeephbac@gmail>
2020-02-15 00:20:31 +0800

    post request

--- deploy.py
+++ deploy.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Heroku + Python Flask API'
+
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data= request.get_json()
+    number_of_sentences = data['number_of_sentences']
+    paragraph = data['paragraph']
+    
+    return  paragraph
+
+
